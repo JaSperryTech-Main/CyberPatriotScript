3ee1dc66077a3460d6cd953bab13f87f63fc1a48
JaSperryTech <jsperry0807@gmail.com>
2025-10-03 13:10:17 -0400

    v2.4

--- main.ps1
+++ main.ps1
@@ -1,8 +1,8 @@
 # main.ps1
 Write-Host "Fetching and running CyberPatriot scripts..." -ForegroundColor Cyan
 
-irm https://raw.githubusercontent.com/JaSperryTech-Main/CyberPatriotScript/main/scripts/Set-LockoutPolicy.ps1 | iex
-irm https://raw.githubusercontent.com/JaSperryTech-Main/CyberPatriotScript/main/scripts/Set-PasswordPolicy.ps1 | iex
-irm https://raw.githubusercontent.com/JaSperryTech-Main/CyberPatriotScript/main/scripts/Set-UserAccountSettings.ps1 | iex
+Invoke-RestMethod https://raw.githubusercontent.com/JaSperryTech-Main/CyberPatriotScript/main/scripts/Set-LockoutPolicy.ps1 | Invoke-Expression
+Invoke-RestMethod https://raw.githubusercontent.com/JaSperryTech-Main/CyberPatriotScript/main/scripts/Set-PasswordPolicy.ps1 | Invoke-Expression
+Invoke-RestMethod https://raw.githubusercontent.com/JaSperryTech-Main/CyberPatriotScript/main/scripts/Set-UserAccountSettings.ps1 | Invoke-Expression
 
 Write-Host "All remote scripts completed!" -ForegroundColor Green
